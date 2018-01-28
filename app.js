39f319de2fcab387c86b5af6bf97a0b1ad7bfb27
ReadyDoMe <unitecomrades777@gmail.com>
2018-01-27 23:22:52 -0600

    added bold text to role response, added auto purge function

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const client = new Discord.Client();
 firebase.initializeApp(keys.google_config);
 const db = firebase.database();
 
+//global
 let authorId;
 let gameInProgress;
 
+//Connection Made
 client.on('ready', ()=> {
     console.log('I am Ready!');
 });
 
+//Messages Listener
 client.on('message', message => {
 
     if (message.content === '!start') {
@@ -123,8 +126,8 @@ client.on('message', message => {
 
                 let percent = (snapshot.val().wins/snapshot.val().games)*100;
                 message.channel.send(
-                    `${message.author}, you have played ${snapshot.val().games} games. You have won ${snapshot.val().wins} and lost ${snapshot.val().losses}.
-                    This gives you a win ratio of ${percent}%`
+                    `${message.author}, you have played **${snapshot.val().games} games**. You have **won ${snapshot.val().wins}** and **lost ${snapshot.val().losses}**.
+                    This gives you a win ratio of **${percent}%**`
                 );
             };
         });
@@ -135,9 +138,23 @@ client.on('message', message => {
     };
 });
 
+//auto-purge comments from general
+client.setInterval ( function(){
+
+    let channel = client.channels.find('name', 'general');
+    let channelMessages = channel.messages;
+
+    channel.bulkDelete(100)
+    .then(messages => channel.message(`Bulk deleted ${messages.size} messages`))
+    .catch(err => console.log(`${err.name}, ${err.message}`))
+
+}, 86400000);
+
+//auto-reconnect
 client.on('disconnect', (err, code) => {
     console.log(`Bot disconnected with code ${code}, for reason ${err}`);
     client.login(keys.bot_token);
 });
 
+//login
 client.login(keys.bot_token);
